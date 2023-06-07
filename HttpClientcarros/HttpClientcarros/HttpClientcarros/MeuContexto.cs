668d4302dca63e7431a96e08cf6e1f71ae077f6a
esdrasluz <84642368+esdrasluz@users.noreply.github.com>
2023-06-07 09:42:59 -0300

    Add files via upload

---
+++ HttpClientcarros/HttpClientcarros/HttpClientcarros/MeuContexto.cs
@@ -0,0 +1,21 @@
+﻿using HttpClientcarros;
+using Microsoft.EntityFrameworkCore;
+
+public class MeuContexto : DbContext
+{
+    public DbSet<veiculo> Veiculos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // Configura a conexão com o banco de dados
+        optionsBuilder.UseSqlServer("sua-string-de-conexao-com-o-banco-de-dados");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Configura o mapeamento das entidades para as tabelas do banco de dados
+        modelBuilder.Entity<veiculo>()
+            .ToTable("Veiculos")
+            .HasKey(v => v.Id); // Supondo que a classe veiculo tenha uma propriedade Id como chave primária
+    }
+}
---
+++ HttpClientcarros/HttpClientcarros/HttpClientcarros/Program.cs
@@ -0,0 +1,45 @@
+﻿//Você pode alterar na url "carros" para "motos" ou "caminhoes" de acordo com a sua necessidade. testes feito usando tabela fipe disponivel no git, testando usando httpcliente e filtro json com C#.
+/* sistema agora com uma nova classe Meucontexto com base de dados generica, dependencias com entity framework e alguma json  deserializado jason para ser lido . */
+using HttpClientcarros;
+using System.Text.Json;
+
+
+Console.WriteLine("Digite o tipo de veiculo!(Carros, Motos ou Caminhões)");
+var TipoVeiculo = Console.ReadLine();
+var endereco = $@"https://parallelum.com.br/fipe/api/v1/{TipoVeiculo}/marcas";
+var client = new HttpClient();
+try
+{
+    HttpResponseMessage? response = await client.GetAsync(endereco);
+    response.EnsureSuccessStatusCode();
+    string responseBody = await response.Content.ReadAsStringAsync();
+    //Console.WriteLine(responseBody);
+    List<veiculo> Veiculos = JsonSerializer.Deserialize<List<veiculo>>(responseBody);
+
+    foreach (var veiculo in Veiculos)
+    {
+        Console.WriteLine(veiculo.nome);
+    }
+
+    
+    using (var contexto = new MeuContexto())
+    {
+        List<veiculo> listaCompleta = new List<veiculo>();
+
+        foreach (var veiculo in Veiculos)
+        {
+            listaCompleta.Add(veiculo);
+            Console.WriteLine(veiculo.nome);
+        }
+
+        contexto.Veiculos.AddRange(listaCompleta);
+
+        contexto.SaveChanges();
+    }
+
+}
+catch (Exception)
+{
+
+    throw;
+}
